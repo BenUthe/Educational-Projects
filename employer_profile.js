593d4bcda0e81b3659df29ddabe258ef01da0221
BenUthe <47396885+BenUthe@users.noreply.github.com>
2019-03-03 17:21:01 -0500

    Add files via upload
    Changed profile to generate based on objects

--- employer_profile.js
+++ employer_profile.js
@@ -1,8 +1,19 @@
 "use strict";
+class employerProfile {
+	constructor(companyName, locate, email, facebook, instagram, twitter, about) {
+		this.companyName = companyName;
+		this.locate = locate;
+		this.email = email;
+		this.facebook = facebook;
+		this.instagram = instagram;
+		this.twitter = twitter;
+		this.about = about;
+	}
+}
 
 const employer = "Company 1";
 let empJobPosts = [];
-let empJobPostsDiv, empNumPostsSpan, empPostForm;
+let empJobPostsDiv, empNumPostsSpan, empPostForm, companyInfo;
 
 function initPage(e) {
 	empJobPostsDiv = document.querySelector("#jobPostings");
@@ -13,6 +24,9 @@ function initPage(e) {
 
 	empJobPosts = getCompanyJobPosts(employer);
 	empJobPosts.forEach(job => renderJobPost(job, empJobPostsDiv, true));
+	
+	companyInfo = getCompanyProfile();
+	fillPage(companyInfo);
 
 	renderNumPosts();
 
@@ -118,4 +132,46 @@ function deleteJobPost(jobPostID) {
 	// Should delete the given job post from db
 	// code below requires server call
 	return true;
+}
+
+function getCompanyProfile() {
+	// Get job posts of company from server
+	// code below requires server call
+	// When we write backend code
+	// pagination will be performed
+	// so only a few posts wil be fetched.
+	
+	const employProfile = new employerProfile(
+		"Company Name",
+		"Toronto, ON",
+		"email@company.com",
+		"https://www.facebook.com",
+		"https://www.instagram.com",
+		"https://www.twitter.com",
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel nibh dictum, " +
+		"feugiat nulla ut, feugiat nibh. Integer a scelerisque mauris, quis consequat " +
+		"Nam et enim id velit maximus rutrum. Fusce nec arcu maximus, consequat risu. " +
+		"Quisque sit amet pellentesque est. Fusce tempus consequat scelerisque. " +
+		"Nunc vel convallis tortor. Phasellus ac condimentum eros. " +
+		"In hac habitasse platea dictumst. Vestibulum at semper dolor. " +
+		"Suspendisse facilisis mollis pellentesque. Nam condimentum varius nunc, " +
+		"ut maximus metus molestie eget. Orci varius natoque penatibus et magnis dis " +
+		"parturient montes, nascetur ridiculus mus. Morbi luctus orci a aliquet malesuada. " +
+		"Suspendisse vitae aliquet lectus."
+	);
+	return employProfile;
+}
+function fillPage(employer){
+	const name = document.getElementById("companyName");
+	name.innerText = employer.companyName;
+	const locate = document.getElementById("locate");
+	locate.innerText = employer.locate;
+	const email = document.getElementById("email");
+	email.innerText = employer.email;
+	const about = document.getElementById("aboutCompany");
+	about.innerText = employer.about;
+	const instagram = document.getElementById("instagram");
+	instagram.href = employer.instagram;
+	const twitter = document.getElementById("twitter");
+	twitter.href = employer.twitter;
 }
